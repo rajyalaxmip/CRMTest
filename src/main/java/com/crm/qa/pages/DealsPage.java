84537b0bef8019e65239ad207786c90facd36601
rpuli <p.rajyalaxmi@gmail.com>
2022-04-27 13:51:56 -0500

    first git push

--- src/main/java/com/crm/qa/pages/DealsPage.java
+++ src/main/java/com/crm/qa/pages/DealsPage.java
@@ -1,5 +1,7 @@
 package com.crm.qa.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import com.crm.qa.base.Testbase;
@@ -9,5 +11,8 @@ public class DealsPage extends Testbase {
 	public DealsPage() {
 		PageFactory.initElements(driver, this);
 	}
+
+	@FindBy(xpath = "//div[@class='ui header item mb5 light-black']")
+	WebElement contactsLabel;
 
 }
